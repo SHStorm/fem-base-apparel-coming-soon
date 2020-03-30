b991183ae36d043e36b5359d7b25ff3beca316fb
SHStorm <digitalsho19@gmail.com>
2020-03-30 16:05:01 +0500

    add form validation

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+const EMAIL_REGEX = /^\S+@\S+\.\S+$/; // custom email regex (not precise)
+
+const $form = document.getElementById('form');
+const $email = $form.elements.namedItem('email');
+const $error = document.getElementById('error');
+
+$form.setAttribute('novalidate', true);
+
+$form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const error = validate(new FormData($form));
+    if (error) {
+        showError(error);
+        $email.focus();
+    } else {
+        hideError();
+    }
+});
+
+function validate(formData) {
+    const email = formData.get('email');
+
+    if (email.trim() === '') {
+        return 'Please provide your email';
+    } else if (!EMAIL_REGEX.test(email)) {
+        return 'Please provide valid email';
+    }
+
+    return null;
+}
+
+function showError(error) {
+    $email.classList.add('invalid');
+    $error.textContent = error;
+    $error.hidden = false;
+}
+
+function hideError() {
+    $email.classList.remove('invalid');
+    $error.textContent = '';
+    $error.hidden = true;
+}
